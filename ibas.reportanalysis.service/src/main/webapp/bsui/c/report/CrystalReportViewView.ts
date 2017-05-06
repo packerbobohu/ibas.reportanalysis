dc91b99a9da8f9876beceb1b009f8b7aa00a80a9
niurenzhu <niuren.zhu@icloud.com>
2017-05-06 12:52:17 +0800

    添加iframe参数

--- ibas.reportanalysis.service/src/main/webapp/bsui/c/report/CrystalReportViewView.ts
+++ ibas.reportanalysis.service/src/main/webapp/bsui/c/report/CrystalReportViewView.ts
@@ -68,11 +68,17 @@ export class CrystalReportViewView extends ibas.View implements ICrystalReportVi
                     ibas.emMessageType.INFORMATION,
                     ibas.i18n.prop("reportanalysis_running_report", data.Value),
                 );
-                let html: string = ibas.strings.format(`<iframe src="{0}" width="100%" height="100%" scrolling="no"></iframe>`, data.Value);
-                // html = ibas.strings.format("");
+                let html: string = ibas.strings.format(
+`<iframe src="{0}" width="100%" height="100%" scrolling="no" seamless
+sandbox="allow-scripts allow-forms"
+></iframe>`, data.Value);
+                // html = ibas.strings.format(`<object style="border:0px" type="text/x-scriptlet" data="{0}" width="100%" height="100%"></object>`);
                 this.form.addContent(
                     new sap.ui.core.HTML("", {
-                        content: html
+                        content: html,
+                        preferDOM: false,
+                        sanitizeContent: true,
+                        visible: true,
                     })
                 );
             }
