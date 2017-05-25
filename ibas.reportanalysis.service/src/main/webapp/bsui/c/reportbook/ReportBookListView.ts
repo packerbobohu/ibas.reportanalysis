3dee94fcd279a26bfacbaf9cfb91d4d1eea8eca4
牛加人等于朱 <niuren.zhu@icloud.com>
2017-05-25 13:17:12 +0800

    修复分页清理历史数据问题。

--- ibas.reportanalysis.service/src/main/webapp/bsui/c/reportbook/ReportBookListView.ts
+++ ibas.reportanalysis.service/src/main/webapp/bsui/c/reportbook/ReportBookListView.ts
@@ -150,10 +150,10 @@ export class ReportBookListView extends ibas.BOListView implements IReportBookLi
         let model: sap.ui.model.Model = this.table.getModel(undefined);
         if (!ibas.objects.isNull(model)) {
             // 已存在绑定数据，添加新的
-            let hDatas: bo.ReportBook[] = (<any>model).getData();
-            if (!ibas.objects.isNull(hDatas) && hDatas instanceof Array) {
+            let hDatas: any = (<any>model).getData();
+            if (!ibas.objects.isNull(hDatas) && hDatas.rows instanceof Array) {
                 for (let item of datas) {
-                    hDatas.push(item);
+                    hDatas.rows.push(item);
                 }
                 model.refresh(false);
                 done = true;
