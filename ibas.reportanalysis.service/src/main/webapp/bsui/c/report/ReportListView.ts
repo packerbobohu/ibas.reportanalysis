45e597907d8a127a55cff25456736c590e3b1800
NiurenZhu <niuren.zhu@icloud.com>
2017-11-16 20:23:15 +0800

    去除空行

--- ibas.reportanalysis.service/src/main/webapp/bsui/c/report/ReportListView.ts
+++ ibas.reportanalysis.service/src/main/webapp/bsui/c/report/ReportListView.ts
@@ -215,7 +215,6 @@ export class ReportListView extends ibas.BOListView implements IReportListView {
     /** 记录上次查询条件，表格滚动时自动触发 */
     query(criteria: ibas.ICriteria): void {
         super.query(criteria);
-
         // 清除历史数据
         if (this.isDisplayed) {
             this.table.setBusy(true);
